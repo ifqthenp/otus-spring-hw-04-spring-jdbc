0b680e5ca542ab39fad238f458dd5096681fa540
Andrei Bogomja <bogomja@gmail.com>
2019-01-20 22:37:54 +0000

    Use lombok for Author entity class

--- src/main/java/com/otus/hw_04/domain/Author.java
+++ src/main/java/com/otus/hw_04/domain/Author.java
@@ -1,44 +1,16 @@
 package com.otus.hw_04.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 
     private long id;
     private String firstName;
     private String lastName;
-
-    public Author(final long id, final String firstName, final String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(final String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(final String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Author{" +
-            "id=" + id +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            '}';
-    }
 
 }
