a1bef07d410f6e08470215c4fe1c806593a13086
Andrei Bogomja <bogomja@gmail.com>
2019-01-22 11:33:13 +0000

    Use lombok for Book entity class

--- src/main/java/com/otus/hw_04/domain/Book.java
+++ src/main/java/com/otus/hw_04/domain/Book.java
@@ -1,66 +1,25 @@
 package com.otus.hw_04.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     private long id;
     private String title;
     private long authorId;
     private long genreId;
-    private String written;
+    private String year;
 
-    public Book(final long id, final String title, final long authorId, final long genreId, final String written) {
-        this.id = id;
+    public Book(final String title, final long authorId, final long genreId, final String year) {
         this.title = title;
         this.authorId = authorId;
         this.genreId = genreId;
-        this.written = written;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public long getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(final long authorId) {
-        this.authorId = authorId;
-    }
-
-    public long getGenreId() {
-        return genreId;
-    }
-
-    public void setGenreId(final long genreId) {
-        this.genreId = genreId;
-    }
-
-    public String getWritten() {
-        return written;
-    }
-
-    public void setWritten(final String written) {
-        this.written = written;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-            "id=" + id +
-            ", title='" + title + '\'' +
-            ", authorId=" + authorId +
-            ", genreId=" + genreId +
-            ", written='" + written + '\'' +
-            '}';
+        this.year = year;
     }
 
 }
