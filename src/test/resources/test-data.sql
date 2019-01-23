ae761f951248374b3e362660bef5785ae6726c26
Andrei Bogomja <bogomja@gmail.com>
2019-01-23 20:40:20 +0000

    Add more test data

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -4,6 +4,8 @@ INSERT INTO authors(first_name, last_name) VALUES ('Miguel', 'de Cervantes');
 INSERT INTO authors(first_name, last_name) VALUES ('Herbert', 'Wells');
 INSERT INTO authors(first_name, last_name) VALUES ('Leo', 'Tolstoy');
 INSERT INTO authors(first_name, last_name) VALUES ('Jane', 'Austen');
+INSERT INTO authors(first_name, last_name) VALUES ('Gabriel', 'García Márquez');
+INSERT INTO authors(first_name, last_name) VALUES ('Leonardo', 'Fibonacci');
 
 INSERT INTO genres(genre) VALUES ('Literary realism');
 INSERT INTO genres(genre) VALUES ('Fantasy');
@@ -11,6 +13,7 @@ INSERT INTO genres(genre) VALUES ('Autobiography');
 INSERT INTO genres(genre) VALUES ('Novel');
 INSERT INTO genres(genre) VALUES ('Science-fiction');
 INSERT INTO genres(genre) VALUES ('Romance');
+INSERT INTO genres(genre) VALUES ('Mathematics');
 
 INSERT INTO books(title, author_id, genre_id, written) VALUES ('Alice in Wonderland', 1, 2, '1865');
 INSERT INTO books(title, author_id, genre_id, written) VALUES ('Jane Eyre', 2, 3, '1847');
@@ -18,3 +21,7 @@ INSERT INTO books(title, author_id, genre_id, written) VALUES ('Don Quixote', 3,
 INSERT INTO books(title, author_id, genre_id, written) VALUES ('The Time Machine', 4, 5, '1895');
 INSERT INTO books(title, author_id, genre_id, written) VALUES ('Anna Karenina', 5, 1, '1878');
 INSERT INTO books(title, author_id, genre_id, written) VALUES ('Pride and Prejudice', 6, 6, '1813');
+INSERT INTO books(title, author_id, genre_id, written) VALUES ('Childhood', 5, 3, '1852');
+INSERT INTO books(title, author_id, genre_id, written) VALUES ('Boyhood', 5, 3, '1854');
+INSERT INTO books(title, author_id, genre_id, written) VALUES ('Love in the Time of Cholera', 7, 4, '1985');
+INSERT INTO books(title, author_id, genre_id, written) VALUES ('The Book of Calculation', 8, 7, '1202');
