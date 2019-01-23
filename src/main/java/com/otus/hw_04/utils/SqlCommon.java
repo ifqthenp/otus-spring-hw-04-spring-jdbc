26720ba88808f542373c97afe8bdb7ccbfeec9e3
Andrei Bogomja <bogomja@gmail.com>
2019-01-23 21:23:43 +0000

    Create utility function for obtaining singleton map with named parameter

---
+++ src/main/java/com/otus/hw_04/utils/SqlCommon.java
@@ -0,0 +1,12 @@
+package com.otus.hw_04.utils;
+
+import java.util.Collections;
+import java.util.Map;
+
+public final class SqlCommon {
+
+    public static Map<String, Object> getNamedParam(final String key, final Object value) {
+        return Collections.singletonMap(key, value);
+    }
+
+}
